522505c366302a24981791d337803784f843b474
AkiraAtom <akirafunakoshi.nestplus2@gmail.com>
2017-02-24 02:30:21 +0900

    tensorflow

---
+++ intron.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+import random
+import math
+import time
+
+start = time.time()
+AC=100
+# number of W
+WN=3
+
+#1+number of the function s dimention
+NN=3
+a = np.arange(0.1,(WN+1)*0.1,0.1)
+
+def y_from_x(_x,_W,_b):
+    _y = np.dot(_W,_x) + _b
+    return _y
+
+x_data = np.random.rand(AC,NN,1).astype("float32")
+y_data = np.zeros((AC,NN,1)).astype("float32")
+npow = 1
+
+for i in xrange(NN):
+    y_data += a[i] * npow
+    npow *= x_data
+
+W = tf.Variable(tf.random_uniform([WN], -1.0, 1.0))
+y = 0
+npow = 1
+
+
+for i in xrange(WN):
+    y += W[i] * npow
+    npow *= x_data
+
+loss = tf.reduce_mean(tf.square(y_data - y))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in xrange(5001):
+    sess.run(train)
+    if step % 100 == 0:
+        print "training...", sess.run(W)
+
+for i in xrange(100):
+    result = 0
+    npow = 1
+
+
+    for j in xrange(WN):
+        result += W[j] * npow
+        npow *= i
+
+    print i, sess.run(result)
+
+sess.close()
+
+timer = time.time() - start
+
+print ("time:{0}".format(timer)) + "[sec]"
+
